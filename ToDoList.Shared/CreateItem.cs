305e034becd7bd17ec7dc913e7ecff6548be5e92
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2022-10-04 14:49:34 +0200

    Validation start

--- ToDoList.Shared/CreateItem.cs
+++ ToDoList.Shared/CreateItem.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,8 @@ namespace ToDoList.Shared
 {
     public class CreateItem
     {
+        [Required]
+        [StringLength(5)]
         public string Text { get; set; } = string.Empty;
     }
 }
