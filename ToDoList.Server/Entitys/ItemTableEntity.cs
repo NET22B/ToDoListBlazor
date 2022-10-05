920160d14c4618c6d0902fbc4025445fe1aa9648
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2022-10-05 14:12:55 +0200

    ItemTableEntity, Mapper

---
+++ ToDoList.Server/Entitys/ItemTableEntity.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Azure.Cosmos.Table;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList.Server.Entitys
+{
+    public class ItemTableEntity : TableEntity
+    {
+        public string Text { get; set; }
+        public bool Completed { get; set; }
+    }
+}
---
+++ ToDoList.Server/Extensions/Mapper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoList.Server.Entitys;
+using ToDoList.Shared;
+
+namespace ToDoList.Server.Extensions
+{
+    public static class Mapper
+    {
+        public static ItemTableEntity ToTableEntity(this Item item)
+        {
+            return new ItemTableEntity
+            {
+                Completed = item.Completed,
+                Text = item.Text,
+                PartitionKey = "Todo",
+                RowKey = item.Id
+            };
+        }
+
+        public static Item ToItem(this ItemTableEntity itemTable)
+        {
+            return new Item
+            {
+                Id = itemTable.RowKey,
+                Text = itemTable.Text,
+                Completed = itemTable.Completed
+            };
+        }
+    }
+}
